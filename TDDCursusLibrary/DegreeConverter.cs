c3fb2e7737513cdda23db43d920547e26ac22530
Helena Copppieters <hcoppieters@hotmail.com>
2021-03-12 09:18:32 +0100

    Oefening DegreeConverterTest class maken

---
+++ TDDCursusLibrary/DegreeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDCursusLibrary
+{
+    public class DegreeConverter
+    {
+        public double ToFahrenheit(double celcius)
+        {
+            return (celcius * 9 / 5) + 32;
+        }
+
+        public double ToCelsius(double fahrenheit)
+        {
+            return (32 * fahrenheit - 32) * 5 / 9;
+        }
+
+    }
+}
